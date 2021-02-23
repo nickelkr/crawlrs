fa4aba6ff05fc213035dffddc567d901c2fbf29f
Kyle Nickel <nickelkr@protonmail.com>
2021-02-22 21:51:06 -0500

    Move fetch into function and test

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,37 @@ struct Opts {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>>{
-    let opts = Opts::from_args();
-    let response = reqwest::get(&opts.url)
+async fn fetch_page(url: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let response = reqwest::get(url)
         .await?
         .text()
         .await?;
+
     println!("{}", response);
     Ok(())
 }
+
+fn main() {
+    let opts = Opts::from_args();
+    let _ = fetch_page(&opts.url);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use mockito::mock;
+    use mockito;
+
+    #[test]
+    fn test_get() {
+        let url = format!("{}/ip", &mockito::server_url());
+        let mock = mock("GET", "/ip")
+            .with_body("yep")
+            .create();
+
+        fetch_page(&url).expect("Failed GET");
+
+        mock.assert();
+    }
+}
+
