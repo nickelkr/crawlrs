f1d97951b15db043535c9564ff2949fc005622f5
Kyle Nickel <nickelkr@protonmail.com>
2021-02-25 22:48:37 -0500

    Move helpers into their own module

--- src/commands.rs
+++ src/commands.rs
@@ -1,9 +1,10 @@
-use regex::Regex;
 use threadpool::ThreadPool;
 
 use std::collections::BTreeSet;
 use std::sync::mpsc::{self, RecvTimeoutError};
 use std::time::Duration;
+
+mod helpers;
 
 pub struct Crawl {
     base_url: String,
@@ -42,10 +43,10 @@ impl Crawl {
             let tx = tx.clone();
 
             pool.execute(move || {
-                let page = fetch_page(&link).expect("GET Failed");
-                let extracted_links = extract_links(page).expect("EXTRACT failed");
+                let page = helpers::fetch_page(&link).expect("GET Failed");
+                let extracted_links = helpers::extract_links(page).expect("EXTRACT failed");
 
-                print_links(&link, &extracted_links);
+                helpers::print_links(&link, &extracted_links);
 
                 for link in extracted_links {
                     tx.send(link).expect("Failed to send msg");
@@ -56,30 +57,6 @@ impl Crawl {
         pool.join();
         println!("Finished");
     }
-}
-
-#[tokio::main]
-async fn fetch_page(url: &str) -> Result<String, Box<dyn std::error::Error>> {
-    println!("DEBUG: fetch page {}", &url);
-    let response = reqwest::get(url).await?.text().await?;
-
-    Ok(response)
-}
-
-fn extract_links(page: String) -> Result<Vec<String>, regex::Error> {
-    let regex = Regex::new("<a href=\"(?P<url>http[s]?://\\S+?)\">")?;
-    let mut links: Vec<String> = vec![];
-
-    for caps in regex.captures_iter(&page) {
-        links.push(caps["url"].to_string());
-    }
-
-    Ok(links)
-}
-
-fn print_links(url: &str, links: &[String]) {
-    let formatted = links.join("\n\t");
-    println!("{}\n\t{}", url, formatted);
 }
 
 #[cfg(test)]
@@ -106,34 +83,5 @@ mod tests {
         crawl.execute();
         mock_index.assert();
         mock_two.assert();
-    }
-
-    #[test]
-    fn test_fetch_page() {
-        let url = format!("{}/", &mockito::server_url());
-        let mock = mock("GET", "/").with_body("body").create();
-        let _ = fetch_page(&url);
-
-        mock.assert();
-    }
-
-    #[test]
-    fn test_extract_links() {
-        let data = "<html>
-        <body>
-            <p>sometext</p>
-            <a href=\"https://somelink.com/blog\">block</a>
-            <p>other text</p>
-            <a href=\"https://anotherlink.com/time\">time</a>
-            <p>end</p>
-        </body>
-        </html>";
-
-        let links = extract_links(data.to_string()).expect("Extract failed");
-
-        assert_eq!(
-            links,
-            vec!["https://somelink.com/blog", "https://anotherlink.com/time"]
-        );
     }
 }
---
+++ src/commands/helpers.rs
@@ -0,0 +1,60 @@
+use regex::Regex;
+
+#[tokio::main]
+pub async fn fetch_page(url: &str) -> Result<String, Box<dyn std::error::Error>> {
+    println!("DEBUG: fetch page {}", &url);
+    let response = reqwest::get(url).await?.text().await?;
+
+    Ok(response)
+}
+
+pub fn extract_links(page: String) -> Result<Vec<String>, regex::Error> {
+    let regex = Regex::new("<a href=\"(?P<url>http[s]?://\\S+?)\">")?;
+    let mut links: Vec<String> = vec![];
+
+    for caps in regex.captures_iter(&page) {
+        links.push(caps["url"].to_string());
+    }
+
+    Ok(links)
+}
+
+pub fn print_links(url: &str, links: &[String]) {
+    let formatted = links.join("\n\t");
+    println!("{}\n\t{}", url, formatted);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use mockito::{self, mock};
+
+    #[test]
+    fn test_fetch_page() {
+        let url = format!("{}/", &mockito::server_url());
+        let mock = mock("GET", "/").with_body("body").create();
+        let _ = fetch_page(&url);
+
+        mock.assert();
+    }
+
+    #[test]
+    fn test_extract_links() {
+        let data = "<html>
+        <body>
+            <p>sometext</p>
+            <a href=\"https://somelink.com/blog\">block</a>
+            <p>other text</p>
+            <a href=\"https://anotherlink.com/time\">time</a>
+            <p>end</p>
+        </body>
+        </html>";
+
+        let links = extract_links(data.to_string()).expect("Extract failed");
+
+        assert_eq!(
+            links,
+            vec!["https://somelink.com/blog", "https://anotherlink.com/time"]
+        );
+    }
+}
